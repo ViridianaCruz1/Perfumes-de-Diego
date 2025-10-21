c6f7de213c8d08c271303f426d8793254ddc6b14
Viridiana_Cruz <viridianacruz2010@gmail.com>
2025-10-21 19:52:09 +0200

    Searchbar como quiso Diego

--- src/ui/SearchBar.jsx
+++ src/ui/SearchBar.jsx
@@ -103,7 +103,7 @@ function SearchBar({ onSearchResult }) {
           value={query}
           onChange={(e) => setQuery(e.target.value)}
           placeholder="Buscar perfume o casa..."
-          className="w-full px-4 py-2 focus:outline-none text-sm h-10 rounded-l-md border border-gray-300"
+          className="w-full px-4 py-2 focus:outline-none text-xs h-10 rounded-l-md border border-gray-300"
           onFocus={() => setShowSuggestions(true)}
         />
         <button
